1ff6d0a2390d668957da6233bfc3dde9fc9fce58
Albert <albert.sun@guantiong.com>
2021-07-30 20:25:42 +0800

    add Static image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 
 const AboutPage = () => {
     return (
-        <Layout pageTitle="About Me">
+        <Layout pageTitle="I'm always-fat.">
             <p>十年磨一剑，岁月不饶人。欢迎来撩，微信公众号：常胖。</p>
+            <StaticImage alt="Clifford" src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg"/>
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
-const IndexPage = () => { 
+const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>本网站由 Gatsby 强力驱动，Gatsby Cloud 托管。</p>
+    <Layout pageTitle="Hello, Gatsby!">
+      <StaticImage alt="Clifford" src="../images/icon.png" />
+      <p>本网站由 Gatsby 强力驱动，使用 Gatsby Cloud 托管。</p>
     </Layout>
   )
 }
